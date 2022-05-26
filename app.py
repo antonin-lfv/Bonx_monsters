c7cc4fc0610a97350caa86181a2e0558d6590ad4
Antonin <antoninlefevre45@icloud.com>
2022-05-26 10:54:10 +0200

    init pages auth errors

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from config import *
+from configuration.config import *
 
 from auth.auth import BLP_auth
 from general.general import BLP_general
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.register_blueprint(BLP_auth)
 app.register_blueprint(BLP_general)
