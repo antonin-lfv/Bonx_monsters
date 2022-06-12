e961f53eb8f36d8f46bb2763363e6ab4093b1b62
Antonin <antoninlefevre45@icloud.com>
2022-06-12 20:57:17 +0200

    update

--- models.py
+++ models.py
@@ -3,7 +3,12 @@ from app import db
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    level = db.Column(db.Integer)
+
+
+class Monster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
