9ead56b2ee34211371e0ee504e69189e3741064f
Antonin <antoninlefevre45@icloud.com>
2022-06-14 11:05:12 +0200

    page monster details

--- general/general.py
+++ general/general.py
@@ -10,6 +10,14 @@ BLP_general = Blueprint('BLP_general', __name__,
                         template_folder='templates',
                         static_folder='static')
 
+def update_power_user(id_user):
+    monsters = Monster.query.filter_by(user_id=id_user).all()
+    user = User.query.filter_by(id=id_user).first()
+    total_power = 0
+    for m in monsters:
+        total_power+=m.power
+    user.power = total_power
+    db.session.commit()
 
 @BLP_general.route('/home', methods=['POST', 'GET'])
 @login_required
@@ -19,9 +27,20 @@ def home():
 @BLP_general.route('/profile', methods=['POST', 'GET'])
 @login_required
 def profile():
+    user = User.query.filter_by(id=current_user.id).first()
     create_and_add_new_monster_from_json("Yellow wizard", current_user.id)
     create_and_add_new_monster_from_json("Red wizard", current_user.id)
     create_and_add_new_monster_from_json("Lord bacus", current_user.id)
     create_and_add_new_monster_from_json("Black mage", current_user.id)
+    update_power_user(id_user=current_user.id)
     monsters = Monster.query.filter_by(user_id=current_user.id).all()
-    return render_template('general/profile.html', user=current_user, monsters=monsters, len=len)
+    return render_template('general/profile.html', user=current_user, monsters=monsters, len=len, int=int)
+
+@BLP_general.route('/monster_details/<id_monster>', methods=['POST', 'GET'])
+@login_required
+def monster_details(id_monster):
+    monster = Monster.query.filter_by(user_id=current_user.id, id=id_monster).first()
+    print(monster)
+    print(id_monster)
+    print(monster.img_path)
+    return render_template('general/monster_details.html', monster=monster)
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-    level = db.Column(db.Integer)
+    power = db.Column(db.Integer)
     coins = db.Column(db.Integer)
     monsters = db.relationship('Monster', backref='user', lazy=True)
 
