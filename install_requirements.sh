c2254abf573b382572c635fe62294592b7223327
Antonin <antoninlefevre45@icloud.com>
2023-12-02 13:32:27 +0100

    handling error with pip and pip3

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,11 @@
 # Install requirements for the project from requirements.txt
 echo "Installing requirements..."
-pip install -r requirements.txt
+# test if pip or pip3 is installed
+if command -v pip3 &>/dev/null; then
+    pip3 install -r requirements.txt
+elif command -v pip &>/dev/null; then
+    pip install -r requirements.txt
+else
+    echo "pip or pip3 not found. Please install pip or pip3."
+    exit 1
+fi
